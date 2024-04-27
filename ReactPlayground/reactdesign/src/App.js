e418bfd26ae12291098109455af22008b5e55cab
Fortune Ndlovu <ndlovufortune97@gmail.com>
2024-04-27 14:31:25 +0100

    Adding routes to my react web app offers a seemless user experiences. after npm installing react-router-dom we impoted the BrowserRouter, Routes and Route components all of the Route  with the specified paths live in the Routes component and each Route renders a component on that specified path. BrowserRouter is the out most compoement.

--- ReactPlayground/reactdesign/src/App.js
+++ ReactPlayground/reactdesign/src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={
+            <Fragment>
+              <Header/>
+              <Home />
+              <Footer />
+            </Fragment>
+          } />
+
+          <Route path='/about' element={
+            <Fragment>
+              <Header />
+              <About />
+              <Footer />
+            </Fragment>
+          } />
+
+          <Route path='/contact' element={
+            <Fragment>
+              <Header />
+              <Contact />
+              <Footer />
+            </Fragment>
+          } />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
