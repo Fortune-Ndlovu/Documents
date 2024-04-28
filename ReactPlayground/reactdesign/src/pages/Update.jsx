f73dfcb0b5fedfa5d0570ae361ae2b0e07cba539
Fortune Ndlovu <ndlovufortune97@gmail.com>
2024-04-28 01:00:11 +0100

    handleFormUpdate

--- ReactPlayground/reactdesign/src/pages/Update.jsx
+++ ReactPlayground/reactdesign/src/pages/Update.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
-import ActivityManager from '../services/ActivityManager'
+import React, { useEffect, useState} from 'react'
+import { db } from '../firebase/firebase';
+import { collection, getDocs, doc } from 'firebase/firestore';
 
 const Update = () => {
+  const [activities, setActivities] = useState([]);
+  useEffect(() => { 
+    const fetchActivities = async () => { 
+      const activitiesCollection = collection(db, "activities");
+      const activitiesDocsRef = await getDocs(activitiesCollection);
+      let activitiesArray = [];
+      activitiesDocsRef.forEach((doc) => {
+        let docObj = { ...doc.data(), id: doc.id }
+        activitiesArray.push(docObj);
+      });
+      setActivities(activitiesArray);
+    }
+    fetchActivities();
+  });
+
+  const handleFormUpdate = (e) => { 
+    e.preventDefault();
+    
+  }
+
   return (
-    <div>Update<ActivityManager /></div>
+    <div>
+      <h1>Update</h1>
+      <form onSubmit={handleFormUpdate}>
+        <label>Name: </label>
+        <input type='text' /><br></br>
+        <label>Description: </label>
+        <input type='text' /><br></br>
+        <button type='submit'>Update</button>
+      </form>
+    </div>
   )
 }
 
